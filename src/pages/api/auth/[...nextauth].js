7bba42b88aa91aca45f80356b0d6cdb46a3e2403
Your Name <you@example.com>
2023-09-17 12:57:26 +1000

    secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -10,13 +10,13 @@ export const authOptions = {
     // ...add more providers here
   ],
 
-  //secret: process.env.NEXTAUTH_SECRET,
-  //secret: process.env.NEXT_PUBLIC_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
 
 
-  //pages: {
-    //signIn: '/auth/signin',
-  //}, 
+  pages: {
+    signIn: '/auth/signin',
+  }, 
 
 }
 export default NextAuth(authOptions)
