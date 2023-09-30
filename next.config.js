b56c8a4d5cf0b741a6c48be56b5fdad694959409
Your Name <you@example.com>
2023-09-30 10:45:25 +1000

    .next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   images: {
-    domains: ["links.papareact.com", "fakestoreapi.com", "drive.google.com", "www.pngitem.com", "www.pngitem.com", "joker-uploads.s3.ap-southeast-2.amazonaws.com"],
+    domains: ["links.papareact.com", "fakestoreapi.com", "drive.google.com", "www.pngitem.com", "joker-uploads.s3.ap-southeast-2.amazonaws.com"],
   },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY
   },
 };
+
+const nextConfig = {
+  distDir: "build", //this line will tell the build to create a file with this name
+};
+
+module.exports = nextConfig;
